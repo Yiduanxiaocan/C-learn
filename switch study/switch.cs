929678ea56c923cae0a69568f817892b6d65501b
Yiduanxiaocan <2895051415@qq.com>
2025-03-01 23:03:46 +0800

    Add files via upload

---
+++ switch study/switch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class switch_study
+{
+    public static void Main(string[]args)
+    {
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch(choice)
+        {
+            case 1:
+                Console.WriteLine("Sports");
+                break;
+            case 2:
+                Console.WriteLine("Business");
+                break;
+            case 3:
+                Console.WriteLine("Technology");
+                break;
+            default:
+                Console.WriteLine("Unknown");
+                break;
+        }
+    }
+}
