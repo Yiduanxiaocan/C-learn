040ae5ea9c32a01effb87db783ab6f42b2f32718
Yiduanxiaocan <2895051415@qq.com>
2025-03-01 20:25:25 +0800

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string color1 = "green";
+        string color2 = "green";
+
+        //your code goes here
+        if (color1 == color2)
+        {
+            Console.WriteLine("great");
+        }
+        else
+        {
+            Console.WriteLine("unknown");
+        }
+    }
+}
