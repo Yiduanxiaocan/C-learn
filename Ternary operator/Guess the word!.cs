d9624a5af30f94ac15b8f3228e6f8a07020bde9f
Yiduanxiaocan <2895051415@qq.com>
2025-03-02 00:23:06 +0800

    Add files via upload

---
+++ Ternary operator/Guess the word!.cs
@@ -0,0 +1,11 @@
+﻿using System;
+    public class guess_//猜单词游戏
+{
+    public static void Main(string[]args)
+    {
+        string answer = Console.ReadLine();
+        string attempt = Console.ReadLine();
+        string result = answer == attempt ? "You win" : "You lose";
+        Console.WriteLine(result);
+    }
+}
---
+++ Ternary operator/Noon Or Midnight.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class Noon_Or_Midnight//利用三元运算符来确定时间段是早晨还是下午
+{
+    public static void Main(string[]args)
+    {
+        int hour = Convert.ToInt32(Console.ReadLine());
+        string result = hour <= 12 ? "AM" : "PM";
+        Console.WriteLine(result);
+    }
+}
