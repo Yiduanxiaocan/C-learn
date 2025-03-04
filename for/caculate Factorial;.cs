3395888069e85a7ea7053934e9c9968b1b85e23a
Yiduanxiaocan <2895051415@qq.com>
2025-03-04 23:43:14 +0800

    Add files via upload

---
+++ for/caculate Factorial;.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Caculate_Factorial
+{
+    static void Main(string[] args)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int Factorial = 1;
+        for(int i = 2; i<=num; i++)
+        {
+            Factorial *= i;
+        }
+        Console.WriteLine(Factorial);
+    }
+}
