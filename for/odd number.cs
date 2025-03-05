5c625d05e5b41678a8dca229e0fad42b12e37fc9
Yiduanxiaocan <2895051415@qq.com>
2025-03-06 01:35:15 +0800

    Add files via upload

---
+++ for/odd number.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Odd
+{
+    public static void Main(string[]args)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        for(int i = 1; i<=number; i+=2)
+        {
+            Console.Write(i+" ");
+        }
+    }
+}
