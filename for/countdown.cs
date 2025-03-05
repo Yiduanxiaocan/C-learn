fb55cd9e1fb9a60fbbd6b9a3820030733a219954
Yiduanxiaocan <2895051415@qq.com>
2025-03-06 01:37:29 +0800

    Add files via upload

---
+++ for/countdown.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Countdown
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+            }while(number >= 0);
+        }
+    }
+}
---
+++ for/sum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Sum
+{
+    class Caculate
+    {
+        static void Main(string[]args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            /*while(num>=0)
+            {
+                sum += num;
+                num--;
+            }
+            Console.WriteLine(sum);*/
+            do
+            {
+                sum += num;
+                num--;
+            } while (num > 0);
+            Console.WriteLine(sum);
+        }
+    }
+}
