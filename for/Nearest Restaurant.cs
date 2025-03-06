5b50f15379c7d8803f6ab8c2c9c83e812a4ce01c
Yiduanxiaocan <2895051415@qq.com>
2025-03-06 23:52:31 +0800

    Add files via upload

---
+++ for/Nearest Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Nearest_restaurant
+{
+    public class Select_restaurant
+    {
+        public static void Main(string[] args)
+        {
+            int floors = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= floors; i++)
+            {
+                if (0 != i % 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ for/break loop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class break_loop
+{
+    public static void Main(string[] args)
+    {
+        int num = 1;
+        for(int i = 1; i<=100; i++)
+        {
+            if(num>=10000)
+            {
+                break;
+             }
+             num *= i;
+         }
+         Console.WriteLine(num);
+     }
+}
