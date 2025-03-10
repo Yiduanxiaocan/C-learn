0be9e62a42751afd5579fe1dfdc768314a0d0dd9
Yiduanxiaocan <2895051415@qq.com>
2025-03-10 20:47:53 +0800

    Add files via upload

---
+++ method/Welcome Sololearner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Welcome_sololearner
+{
+    class Program
+    {
+        static void Main()
+        {
+            Welcome();
+        }
+        static void Welcome()
+        {
+            string? name = Console.ReadLine();
+            if (name != null)
+            {
+                Console.WriteLine($"Welcome, {name}!");
+            }
+            else
+            {
+                Console.WriteLine("Welcome, guest!");
+            }
+        }
+    }
+}
